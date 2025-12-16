1d9affa32dae7e63e1fe6487e0f7dab0314cbc81
guiisbarbosa <gui.silva93@hotmail.com>
2025-12-16 20:53:18 -0300

    feat: Inicialização do projeto e criação Header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-(--bg-page)">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
