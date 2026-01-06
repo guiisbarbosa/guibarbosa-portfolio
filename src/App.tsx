bc9f0898b90c78ce198b477b3527002b4a073c59
guiisbarbosa <gui.silva93@hotmail.com>
2026-01-06 09:44:35 -0300

    correção de branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from "react";
 import { Header } from "@components/sections/Header/Header";
 import { Hero } from "@components/sections/Hero/Hero";
 
 function App() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-      <div className="min-h-screen bg-(--bg-page)">
-        <Header />
+      <div
+        className={`min-h-screen bg-(--bg-page) ${isScrolled ? "pt-20" : ""}`}
+      >
+        <Header isScrolled={isScrolled} />
         <Hero />
       </div>
     </>
