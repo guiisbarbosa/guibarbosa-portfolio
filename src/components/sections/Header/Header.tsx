894687cae9e4bc765c3b3d120d6528edad475b88
guiisbarbosa <gui.silva93@hotmail.com>
2026-01-06 09:43:34 -0300

    correção de branch

--- src/components/sections/Header/Header.tsx
+++ src/components/sections/Header/Header.tsx
@@ -1,28 +1,50 @@
 import { FaRegMoon } from "react-icons/fa6";
 
-export const Header = () => {
+interface HeaderProps {
+  isScrolled: boolean;
+}
+
+export const Header = ({ isScrolled }: HeaderProps) => {
   return (
-    <header className="flex w-full justify-between items-center px-8 py-4 bg-(--bg-header)">
+    <header
+      className={`flex w-full justify-between items-center px-8 py-4 transition-all duration-300 ${
+        isScrolled
+          ? "fixed top-0 left-0 right-0 z-50 bg-(--bg-header)/80 backdrop-blur-md shadow-lg"
+          : "bg-(--bg-header)"
+      }`}
+    >
       <h1 className="text-3xl font-bold text-(--highlight)">GB</h1>
 
       <nav className="">
         <ul className="text-white flex gap-4 items-center">
-          <li className="hover:text-(--highlight) transition-colors">
-            <a href="#">Início</a>
-          </li>
-          <li className="hover:text-(--highlight) transition-colors">
-            <a href="#">Sobre</a>
-          </li>
-          <li className="hover:text-(--highlight) transition-colors">
-            <a href="#">Habilidades</a>
+          <li>
+            <a href="#" className="menu-link">
+              Início
+            </a>
           </li>
           <li>
-            <a href="#">Projetos</a>
+            <a href="#" className="menu-link">
+              Sobre
+            </a>
           </li>
-          <li className="hover:text-(--highlight) transition-colors">
-            <a href="#">Contato</a>
+          <li>
+            <a href="#" className="menu-link">
+              Habilidades
+            </a>
           </li>
-          <li className="hover:text-(--highlight) transition-colors text-2xl">
+          <li>
+            <a href="#" className="menu-link">
+              Projetos
+            </a>
+          </li>
+          <li>
+            <a href="#" className="menu-link">
+              Contato
+            </a>
+          </li>
+          <li
+            className={`hover:text-(--highlight) transition-colors text-2xl cursor-pointer`}
+          >
             <FaRegMoon />
           </li>
         </ul>
