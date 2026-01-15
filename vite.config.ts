f508473ba16e29862f6c374ad82d64eb3641c156
guiisbarbosa <gui.silva93@hotmail.com>
2026-01-15 15:42:41 -0300

    chroe: Instalação de dependências para funcionamento do Carousel do Shadcn UI e correção do alias nas configs

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname, "src/components"),
+      "@": path.resolve(__dirname, "./src/"),
+      "@components": path.resolve(__dirname, "./src/components"),
       "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
